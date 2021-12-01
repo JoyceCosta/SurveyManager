e711dbc43448d3810fdd5a3ad41ed32a1f976632
Joyce Costa <49466938+JoyceCosta@users.noreply.github.com>
2021-12-01 11:57:42 -0300

    Add files via upload

---
+++ Option.cs
@@ -0,0 +1,48 @@
+using System;
+using System.IO;
+
+namespace SurveyManeger
+{
+	
+	/// Option of a survey.
+	class Option : IStorable, IEquatable<Option>
+	{
+		/// Option ID (which must be entered to choose the option).
+		public string? Id { get; set; }
+
+		/// Text associated with the option.
+		public string? Text { get; set; }
+
+		public void Save(BinaryWriter writer)
+		{
+			writer.Write(Id);
+			writer.Write(Text);
+		}
+
+		public void Load(BinaryReader reader)
+		{
+			Id = reader.ReadString();
+			Text = reader.ReadString();
+		}
+
+		public override bool Equals(object? obj)
+		{
+			return Equals(obj as Option);
+		}
+
+		public bool Equals(Option other)
+		{
+			if (other == null)
+			{
+				return false;
+			}
+
+			return this.Id == other.Id;
+		}
+
+		public override int GetHashCode()
+		{
+			return Id.GetHashCode();
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SurveyManeger
+{
+	class Program
+	{
+		static void Main()
+		{
+			// Starts program execution.
+			SurveyUI ui = new SurveyUI();
+			ui.Start();
+		}
+	}
+}
