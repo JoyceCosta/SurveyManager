2b18d830558477173f18ec29f3f515f361ddc39a
Joyce Costa <49466938+JoyceCosta@users.noreply.github.com>
2021-12-01 12:03:10 -0300

    Update SurveyiUI.cs

--- SurveyiUI.cs
+++ SurveyiUI.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 
-namespace SurveyManeger
+namespace SurveyManager
 {
 	/// Manages the application's graphical interface.
 	class SurveyUI
